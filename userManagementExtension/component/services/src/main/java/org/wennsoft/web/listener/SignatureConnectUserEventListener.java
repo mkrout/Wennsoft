aa9a5c745af910fcc4969548af2be061ac9f86b1
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2013-08-06 14:42:18 +0100

    [6589]: Maintaining User Records Should Create Audit Entries
    Signed-off-by: Ayoub Zayati <ayoub.ezzayati@exoplatform.com>

---
+++ userManagementExtension/component/services/src/main/java/org/wennsoft/web/listener/SignatureConnectUserEventListener.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package org.wennsoft.web.listener;
+
+import org.exoplatform.services.organization.User;
+import org.exoplatform.services.organization.UserEventListener;
+import org.gatein.common.logging.Logger;
+import org.gatein.common.logging.LoggerFactory;
+import org.exoplatform.portal.webui.util.Util;
+
+/**
+ * @author Ayoub Zayati
+ */
+public class SignatureConnectUserEventListener extends UserEventListener {
+
+	private static final Logger log = LoggerFactory.getLogger(SignatureConnectUserEventListener.class);
+
+    public void postSave(User user, boolean isNew) throws Exception {
+        
+        String userName = user.getUserName();
+        String currentUserName = Util.getPortalRequestContext().getRemoteUser();
+        if (isNew) { // user added
+        	log.info("[User Management] New user " + userName + " created by " + currentUserName);
+        	
+        } else {// user updated
+        	log.info("[User Management] " + currentUserName + " modified user " + userName);
+        	
+        }
+    }
+}    
