b596fa40d2066573eaa57a5f3d434857c8f33b1b
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2013-08-13 13:01:36 +0100

    [6589]: Maintaining User Records Should Create Audit Entries (Review code)

--- userManagementExtension/component/services/src/main/java/org/wennsoft/web/listener/SignatureConnectUserEventListener.java
+++ userManagementExtension/component/services/src/main/java/org/wennsoft/web/listener/SignatureConnectUserEventListener.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package org.wennsoft.web.listener;
 
 import org.exoplatform.services.organization.User;
@@ -12,30 +9,37 @@ import org.exoplatform.portal.webui.util.Util;
 /**
  * @author Ayoub Zayati
  */
-public class SignatureConnectUserEventListener extends UserEventListener {
+public class SignatureConnectUserEventListener extends UserEventListener 
+{
+    private static final Log LOGGER = ExoLogger.getLogger(SignatureConnectUserEventListener.class);
 
-	private static final Log log = ExoLogger.getLogger(SignatureConnectUserEventListener.class);
-
-    public void postSave(User user, boolean isNew) throws Exception {
-    	String userName = user.getUserName();
-        try {
+    public void postSave(User user, boolean isNew) throws Exception 
+    {
+        String userName = user.getUserName();
+        try 
+        {
             String currentUserName = Util.getPortalRequestContext().getRemoteUser();
-            if (isNew) { // user added
-            	log.info("[User Management] New user " + userName + " created by " + currentUserName);
-            	
-            } else {// user updated
-            	log.info("[User Management] " + currentUserName + " modified user " + userName);
-            	
+            if (isNew) 
+            { // user added
+                LOGGER.info("[User Management] New user " + userName + " created by " + currentUserName);
+            } 
+            else 
+            {// user updated
+                LOGGER.info("[User Management] " + currentUserName + " modified user " + userName);
             }
 			
-		} catch (NullPointerException npe) {
-			 if (isNew) { // user added
-	            	log.info("[User Management] New user " + userName + " created");
-	            	
-	         } else {// user updated
-	            	log.info("[User Management] " + userName + " modified");
-	         }
+		} 
+        catch (NullPointerException npe) 
+        {
+		    if (isNew) 
+		    { // user added
+		        LOGGER.info("[User Management] New user " + userName + " created");
+	        } 
+		    else 
+		    {// user updated
+	            LOGGER.info("[User Management] " + userName + " modified");
+	        }
 		}
         
     }
-}    
+}
